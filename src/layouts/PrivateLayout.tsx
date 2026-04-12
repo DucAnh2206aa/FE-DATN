12c30cde38549697db4dfe08af7c53d1ae4f51bc
TranTu183 <tutaph41643@fpt.edu.vn>
2026-04-12 22:37:42 +0700

    update nhỏ

--- src/layouts/PrivateLayout.tsx
+++ src/layouts/PrivateLayout.tsx
@@ -368,13 +368,13 @@ export const PrivateLayout = () => {
                         icon: <TeamOutlined />,
                         label: <Link to={ROUTE_PATHS.DASHBOARD_ACCOUNTS}>Tài khoản</Link>,
                       },
-                      {
-                        key: MENU_KEYS.CHATBOT_PRESETS,
-                        icon: <RobotOutlined />,
-                        label: (
-                          <Link to={ROUTE_PATHS.DASHBOARD_CHATBOT_PRESETS}>Kịch bản chatbot</Link>
-                        ),
-                      },
+                      // {
+                      //   key: MENU_KEYS.CHATBOT_PRESETS,
+                      //   icon: <RobotOutlined />,
+                      //   label: (
+                      //     <Link to={ROUTE_PATHS.DASHBOARD_CHATBOT_PRESETS}>Kịch bản chatbot</Link>
+                      //   ),
+                      // },
                       {
                         key: MENU_KEYS.ROLES,
                         icon: <SafetyCertificateOutlined />,
